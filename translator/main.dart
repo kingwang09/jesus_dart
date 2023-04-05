81c24ad825555e885eef6722d9c27dd44298d19a
hyungeun.jin <kingwang09@gmail.com>
2023-04-05 17:47:00 +0900

    구글 번역에 대한 무료 사용

---
+++ translator/main.dart
@@ -0,0 +1,26 @@
+import 'package:translator/translator.dart';
+
+void main() async {
+  final translator = GoogleTranslator();
+  final input = "Здравствуйте. Ты в порядке?";
+
+  // Using the Future API
+  translator
+      .translate(input, to: 'ko')
+      .then((result) => print("Source: $input\nTranslated: $result"));
+
+  // Passing the translation to a variable
+  var translation = await translator
+      .translate("I would buy a car, if I had money.", from: 'en', to: 'ko');
+
+  // You can also call the extension method directly on the input
+  print('Translated: ${await input.translate(to: 'ko')}');
+
+  // For countries that default base URL doesn't work
+  // translator.baseUrl = "translate.google.cn";
+  // translator.translateAndPrint("This means 'testing' in chinese", to: 'zh-cn');
+  //prints 这意味着用中文'测试'
+
+  print("translation: $translation");
+  // prints translation: Vorrei comprare una macchina, se avessi i soldi.
+}
