474584d86822013b967d8c79ee6410ccb32c855b
hyungeun.jin <kingwang09@gmail.com>
2023-04-05 18:10:04 +0900

    배열 테스트

---
+++ basic/array.dart
@@ -0,0 +1,28 @@
+
+void firstArray(){
+  var flybyObjects = ['Jupiter', 'Saturn', 'Uranus', 'Neptune'];
+
+  //조건문으로 필터링 후 forEach
+  flybyObjects.where((name) => name.contains('turn')).forEach(print);
+  //where를 한다해서 원본이 바뀌지는 않는 듯. 새로운 배열을 반환하는 것으로 보임.
+
+
+  flybyObjects.forEach((element) {
+    print("long print: $element");
+  });
+
+  //위 내용을 아래처럼 간략히 표현 가능
+  flybyObjects.forEach(print);
+
+  var jupiter = flybyObjects.where((name) => name=='Jupiter');//신규 배열로 반환되게됨.
+  print("jupiter: $jupiter");
+}
+
+void secondArray(){
+  print("안녕");
+}
+
+void main(){
+  //배열과의 첫만남
+  //firstArray();
+}
