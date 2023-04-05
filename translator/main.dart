64f23da25968f9925f0b383b9d95e0c428733d92
hyungeun.jin <kingwang09@gmail.com>
2023-04-05 17:55:17 +0900

    구글 번역에 대한 무료 사용 예제 -> 코멘트 추가

--- translator/main.dart
+++ translator/main.dart
@@ -4,23 +4,16 @@ void main() async {
   final translator = GoogleTranslator();
   final input = "Здравствуйте. Ты в порядке?";
 
-  // Using the Future API
-  translator
-      .translate(input, to: 'ko')
+  // Using the Future API: 비동기 방식
+  translator.translate(input, to: 'ko')
       .then((result) => print("Source: $input\nTranslated: $result"));
 
-  // Passing the translation to a variable
-  var translation = await translator
-      .translate("I would buy a car, if I had money.", from: 'en', to: 'ko');
+  // Passing the translation to a variable: 동기 방식
+  var translation = await translator.translate("I would buy a car, if I had money.", from: 'en', to: 'ko');
+  print("translation: $translation");
 
   // You can also call the extension method directly on the input
+  //string으로 바로 처리가 가능하네?ㅎ
   print('Translated: ${await input.translate(to: 'ko')}');
 
-  // For countries that default base URL doesn't work
-  // translator.baseUrl = "translate.google.cn";
-  // translator.translateAndPrint("This means 'testing' in chinese", to: 'zh-cn');
-  //prints 这意味着用中文'测试'
-
-  print("translation: $translation");
-  // prints translation: Vorrei comprare una macchina, se avessi i soldi.
 }
