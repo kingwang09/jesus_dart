7288fbf37c13ae907a0ea8e6cde941753b7a312e
hyungeun.jin <kingwang09@gmail.com>
2023-04-06 14:30:55 +0900

    클래스에 대한 기본 공부(final 변수 추가)

--- object/main.dart
+++ object/main.dart
@@ -1,6 +1,8 @@
 class Hello{
   int? age;
   String? name;
+  final String bornCountry = "korea";
+
   //생성자 사용
   //명시적 생성자
   //상수 생성자
@@ -8,10 +10,10 @@ class Hello{
 
 void main(){
   var hello = Hello();
-  print("age: ${hello.age}, name=${hello.name}"); //npe는 없는듯.
+  print("age: ${hello.age}, name=${hello.name}, born=${hello.bornCountry}"); //npe는 없는듯.
   hello.age = 35;
   hello.name = "다니엘";
 
   print(hello);
-  print("age: ${hello.age}, name=${hello.name}");
+  print("age: ${hello.age}, name=${hello.name}, born=${hello.bornCountry}");
 }
