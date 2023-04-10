c0dd7f72c510cebeec7d2b6a40ddf2c9ecc47587
hyungeun.jin <kingwang09@gmail.com>
2023-04-10 10:15:34 +0900

    collection > iterable 기초

---
+++ collection/main.dart
@@ -0,0 +1,27 @@
+void main(){
+  //iterable
+    //list, map, set
+
+  Iterable<int> iterable = const [1, 2, 3, 4, 5, 6, 7];
+  int fiveOver = iterable.firstWhere((element) => element > 5);
+  print("result five over: $fiveOver");
+
+  //fist, last element
+  int first = iterable.first;
+  int last = iterable.last;
+  print("first: $first, last: $last");
+
+  //int fiveOverOne = iterable.singleWhere((element) => element > 5); //to many elements exceptions
+  //print("result five over one: $fiveOverOne");
+
+  //isContains: every(), any()
+  bool isFiveOverEvery = iterable.every((element) => element > 5);
+  print("is five over(every): $isFiveOverEvery");
+
+  bool isFiveOverAny = iterable.any((element) => element > 5);
+  print("is five over(any): $isFiveOverAny");
+
+  //filtering .where()
+
+
+}
